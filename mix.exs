467a2f0668dff6fd851afaf1ad4a49a9ae01cdde
Michael Quinlan <mjquinlan2000@gmail.com>
2026-05-21 12:29:54 -0600

    Updating the decimal deps

--- mix.exs
+++ mix.exs
@@ -79,7 +79,7 @@ defmodule Absinthe.Mixfile do
       {:telemetry, "~> 1.0 or ~> 0.4"},
       {:credo, "~> 1.1", only: [:dev, :test], runtime: false, override: true},
       {:dataloader, "~> 1.0.0 or ~> 2.0", optional: true},
-      {:decimal, "~> 2.0", optional: true},
+      {:decimal, "~> 3.0", optional: true},
       {:opentelemetry_process_propagator, "~> 0.3 or ~> 0.2.1", optional: true},
       {:ex_doc, "~> 0.22", only: :dev},
       {:benchee, ">= 1.0.0", only: :dev},
